708096732430fe97c999e4ddd8ba1e8f6763b153
Hyonsong Lee <hynsngl@gmail.com>
2020-04-17 17:33:24 +0900

    Configure linter and editor and structure project
    • Change linter to Prettier in .eslintrc.js
    • Create .prettierrc.js to configure Prettier
    • Create global.scss in assets directory for global style setup
    • Create AppNav, EventCard, and Dashboard and update Home
    • Add paths for Home and Dashboard to Router
    • Create events.json and user.json

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Dashboard from '@/views/Dashboard.vue'
 
 Vue.use(VueRouter);
 
@@ -11,18 +12,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/dashboard",
+    name: 'dashboard',
+    component: Dashboard
   }
-];
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
